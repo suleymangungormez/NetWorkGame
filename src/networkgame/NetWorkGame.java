6056512f079a57b8168cde2eb6ad49904b4bab25
suleymangungormez <Osx@Osx-MacBook-Pro-2.local>
2017-03-09 08:56:33 +0200

    proje oluşturuldu

---
+++ src/networkgame/NetWorkGame.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package networkgame;
+
+/**
+ *
+ * @author Osx
+ */
+public class NetWorkGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
